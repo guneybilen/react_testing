1dbcd06924f248e565902ab7c979c33107e3ad21
guney bilen <guneybilen@yahoo.com>
2018-04-02 13:44:37 +0300

    initial commit

---
+++ test/components/app_test.js
@@ -0,0 +1,29 @@
+import { renderComponent , expect } from '../test_helper';
+import App from '../../src/components/app';
+
+//Use 'describe' to group together similar tests
+describe('App (this string is for documentation)' , () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+
+  // Use 'it' to test a single attribute of a target
+  it('renders something', () => {
+    //Use 'expect' to make an assertion about the target 
+    //(App Component in this describe block)
+    expect(component).to.exist;
+  });
+
+  /* 
+  it('shows the correct text (this string is for documentation)', () => {    
+    expect(component).to.contain('React simple starter');
+   });  
+  */
+
+  it('shows a comment box', () => {    
+    expect(component.find('.comment-box')).to.exist;
+  });
+
+});
---
+++ test/components/comment_box_test.js
@@ -0,0 +1,51 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+import { Z_BLOCK } from 'zlib';
+
+describe('CommentBox', () => {
+ 
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
+  it('has the correct class', () => {
+    // const component = renderComponent(CommentBox);
+    expect(component).to.have.class('comment-box');
+  });
+
+  it('has a text area', () => {
+    // const component = renderComponent(CommentBox);
+    expect(component.find('textarea')).to.exist;
+  });
+
+  it('has a button', () => {
+    // const component = renderComponent(CommentBox);
+    expect(component.find('button')).to.exist;
+  });
+
+
+  // closely related tests should be in their own describe block.
+  // beforeEach on top still going to run before the each it block
+  // in the following describe blocks. After the top beforeEach
+  // Mocha will run the following describe's child beforeEach.
+  describe('entering some text', () => {
+
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+  
+    it('shows that text is in the textarea', () => {
+      // const component = renderComponent(CommentBox);
+      expect(component.find('button')).to.exist;
+    });
+  
+    it('when submitted clears the input', () => {
+      // const component = renderComponent(CommentBox);
+      expect(component.find('button')).to.exist;
+    });
+  
+  })
+  
+});
